5ddb894fb2cb225935cc275901bb0a844e364377
mjsipes <msipes@usc.edu>
2024-07-11 23:50:19 -0700

    lets try on mackbook air

---
+++ js.js
@@ -0,0 +1,14 @@
+function name(params) {
+
+    
+}
+//write a function that takes in a string and returns the string with all capital letters
+//example: shout("hello") --> "HELLO"
+
+function shout(string) {
+    return string.toUpperCase(); // Fixed typo in method name
+}
+
+
+//use the shout function
+shout("hello");
